947466f5aeabd1e18be6363b4dd18e93c327e3f8
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2016-11-25 20:19:24 +0100

    Add user info to Media

--- src/Media.elm
+++ src/Media.elm
@@ -11,15 +11,31 @@ type alias Media =
     { url : String
     , created_time : String
     , id : String
+    , user : User
+    }
+
+
+type alias User =
+    { username : String
+    , profile_picture : String
+    , id : String
     }
 
 
 mediaDecoder : Decoder Media
 mediaDecoder =
-    Decode.map3 Media
+    Decode.map4 Media
         (at [ "images", "standard_resolution", "url" ] Decode.string)
         (at [ "created_time" ] Decode.string)
         (at [ "id" ] Decode.string)
+        (at [ "user" ]
+            (Decode.map3
+                User
+                (at [ "username" ] Decode.string)
+                (at [ "profile_picture" ] Decode.string)
+                (at [ "id" ] Decode.string)
+            )
+        )
 
 
 mediaListDecoder : Decoder (List Media)
@@ -39,6 +55,15 @@ get apiHost token id =
 
 view data =
     div [ class "card" ]
-        [ img [ style [ ( "width", "100%" ) ], class "card-img-top", src data.url ] []
+        [ div [ class "card-block" ]
+            [ img
+                [ class "rounded-circle"
+                , style [ ( "width", "50px" ), ( "margin-right", "15px" ) ]
+                , src data.user.profile_picture
+                ]
+                []
+            , text data.user.username
+            ]
+        , img [ style [ ( "width", "100%" ) ], class "card-img-top", src data.url ] []
         , div [ class "card-block" ] [ text data.id ]
         ]
