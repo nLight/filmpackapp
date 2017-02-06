9bb28f63a08787489a821d51267e217a46d09c37
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2017-02-06 18:17:45 +0100

    Switch addButton label depending on number of accounts

--- src/Main.elm
+++ src/Main.elm
@@ -222,23 +222,32 @@ emptyStream =
     }
 
 
-addStream =
-    div [ class "col-xs-4" ]
-        [ div []
+addStream streams =
+    let
+        buttonText =
+            case Dict.size streams of
+                0 ->
+                    "Login"
+
+                _ ->
+                    "Add account"
+    in
+        div [ class "col-xs-4" ]
             [ div []
-                [ div [ class "media", style [ ( "margin", "30px 0" ) ] ]
-                    [ a [ class "media-left" ] [ div [ style [ ( "width", "100px" ), ( "height", "100px" ) ], class "bg-faded rounded-circle" ] [] ]
-                    , div [ class "media-body align-middle" ]
-                        [ a
-                            [ class "btn btn-outline-primary"
-                            , href "https://api.instagram.com/oauth/authorize/?scope=public_content+follower_list+comments+relationships+likes&client_id=a59977aae66341598cb366c081e0b62d&redirect_uri=http://packfilmapp.com&response_type=token"
+                [ div []
+                    [ div [ class "media", style [ ( "margin", "30px 0" ) ] ]
+                        [ a [ class "media-left" ] [ div [ style [ ( "width", "100px" ), ( "height", "100px" ) ], class "bg-faded rounded-circle" ] [] ]
+                        , div [ class "media-body align-middle" ]
+                            [ a
+                                [ class "btn btn-outline-primary"
+                                , href "https://api.instagram.com/oauth/authorize/?scope=public_content+follower_list+comments+relationships+likes&client_id=a59977aae66341598cb366c081e0b62d&redirect_uri=http://packfilmapp.com&response_type=token"
+                                ]
+                                [ text buttonText ]
                             ]
-                            [ text "Login" ]
                         ]
                     ]
                 ]
             ]
-        ]
 
 
 streamsView data =
@@ -279,7 +288,7 @@ appView model =
                 [ div [ class "col-xs-12" ] (messagesView model.messages)
                 ]
             , div [ class "row" ]
-                ((streamsView model.streams) ++ [ addStream ])
+                ((streamsView model.streams) ++ [ addStream model.streams ])
             ]
         , nav [ class "navbar navbar-full navbar-light bg-faded" ]
             [ div [ class "col-xs-12" ]
